ad0ace353e70df499edcad284f175fdd81290a57
DaminboevM <m701rizo@gmail.com>
2025-07-25 19:27:31 +0500

    added  dotenv

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(process.cwd(), 'uploads/public'), { prefix: '/files/public', });
-
   await setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
+
+
 bootstrap();
