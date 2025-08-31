db7c4d43fb663c12f33af93d05f25da221aed4dc
DaminboevM <m701rizo@gmail.com>
2025-08-31 16:06:39 +0500

    updated contact-module

--- src/modules/contact/contact.module.ts
+++ src/modules/contact/contact.module.ts
@@ -3,9 +3,10 @@ import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
 import { JwtAccsesToken } from 'src/common/config/jwt/jwt';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/Database/prisma.module';
 
 @Module({
-  imports: [JwtModule.register(JwtAccsesToken)],
+  imports: [PrismaModule, JwtModule.register(JwtAccsesToken)],
   controllers: [ContactController],
   providers: [ContactService]
 })
